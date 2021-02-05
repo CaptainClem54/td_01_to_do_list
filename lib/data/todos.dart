4638ce0c9437f59e0aa3d5ab349b55e9c346e428
Clément MACHERET <clement.macheret@viacesi.fr>
2021-02-05 08:42:36 +0100

    List ToDo

---
+++ lib/data/todos.dart
@@ -0,0 +1,26 @@
+import 'package:td_01_to_do_list/models/Todo.dart';
+
+final aTodo = Todo(
+  title: "Aller aux foot",
+  content: "Match avec Bernard",
+  done: false,
+);
+
+List<Todo> todos = [
+  Todo(title: "Todo1", content: "descriptionToDo", done: true),
+  Todo(title: "Todo2", content: "descriptionToDo", done: true),
+  Todo(title: "Todo3", content: "descriptionToDo", done: true),
+  Todo(title: "Todo4", content: "descriptionToDo", done: true),
+  Todo(title: "Todo5", content: "descriptionToDo", done: true),
+  Todo(title: "Todo6", content: "descriptionToDo", done: true),
+  Todo(title: "Todo7", content: "descriptionToDo", done: true),
+  Todo(title: "Todo8", content: "descriptionToDo", done: true),
+  Todo(title: "Todo9", content: "descriptionToDo", done: true),
+  Todo(title: "Todo10", content: "descriptionToDo", done: true),
+  Todo(title: "Todo11", content: "descriptionToDo", done: true),
+  Todo(title: "Todo12", content: "descriptionToDo", done: true),
+  Todo(title: "Todo13", content: "descriptionToDo", done: true),
+  Todo(title: "Todo14", content: "descriptionToDo", done: true),
+  Todo(title: "Todo15", content: "descriptionToDo", done: true),
+  Todo(title: "Todo16", content: "descriptionToDo", done: true),
+];
